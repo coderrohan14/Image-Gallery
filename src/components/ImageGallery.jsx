c86fb934fd4436b0ee781427fa5e1eb8bb3e6dcd
coderrohan14 <Rahkins@1996>
2023-02-25 18:57:49 +0530

    import changes

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./appStyles.css";
 import ImageCard from "./ImageCard";
-import ImagePopUp from "./ImagePopUp";
 
 const ImageGallery = ({ itemData, openPopUp }) => {
   return (
